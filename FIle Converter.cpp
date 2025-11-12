8dcb5409ace1d8fb451124e2533e0128a6eac82e
Yoonho Kim <yunhogim162@gmail.com>
2025-11-13 00:04:37 +1200

    accept file path from the user

--- FIle Converter.cpp
+++ FIle Converter.cpp
@@ -1,38 +1,77 @@
 #include <wx/wx.h>
+#include <wx/filedlg.h>
+#include <wx/textfile.h>
 
-// Application class
+
+// App class
 class MyApp : public wxApp
 {
 public:
     virtual bool OnInit();
 };
 
-// Main frame class
+// Main frame
 class MyFrame : public wxFrame
 {
 public:
     MyFrame(const wxString& title);
+
+private:
+    void OnExit(wxCommandEvent& event);
+    void OnOpenFile(wxCommandEvent& event);
 };
+
+wxIMPLEMENT_APP(MyApp);
 
 // App init
 bool MyApp::OnInit()
 {
-    MyFrame* frame = new MyFrame("Hello wxWidgets!");
+    MyFrame* frame = new MyFrame("My wxWidgets Notepad");
     frame->Show(true);
     return true;
 }
 
-// Frame constructor
+// Main frame constructor
 MyFrame::MyFrame(const wxString& title)
-    : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(400, 300))
+    : wxFrame(nullptr, wxID_ANY, title, wxDefaultPosition, wxSize(600, 400))
 {
-    wxPanel* panel = new wxPanel(this, wxID_ANY);
-    wxButton* btn = new wxButton(panel, wxID_ANY, "Click Me", wxPoint(150, 100), wxSize(100, 30));
+    // Menu bar
+    wxMenu* fileMenu = new wxMenu;
+    fileMenu->Append(wxID_EXIT, "&Exit\tCtrl+Q", "Quit the application");
+    fileMenu->Append(wxFD_OPEN, "&Open File\tCtrl+O", "Open a file");
 
-    // event when the button has been clicked
-    btn->Bind(wxEVT_BUTTON, [this](wxCommandEvent&) {
-        wxMessageBox("Hello, wxWidgets!", "Message", wxOK | wxICON_INFORMATION);
-        });
+    wxMenuBar* menuBar = new wxMenuBar;
+    menuBar->Append(fileMenu, "&File");
+
+    SetMenuBar(menuBar);
+
+    //Create button
+    wxPanel* panel = new wxPanel(this, wxID_ANY);
+    wxButton* openfile = new wxButton(panel, wxID_ANY, "Select File", wxPoint(120, 60), wxSize(150, 40));
+
+    // Event binding
+    openfile->Bind(wxEVT_BUTTON, &MyFrame::OnOpenFile, this);
+    Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
+    Bind(wxEVT_MENU, &MyFrame::OnOpenFile, this, wxFD_OPEN);
 }
 
-wxIMPLEMENT_APP(MyApp);
+// Open file
+void MyFrame::OnOpenFile(wxCommandEvent& event) {
+    wxFileDialog openFileDialog(
+        this, _("Select a file"), "", "",
+        "PPT|*.pptx",
+        wxFD_OPEN | wxFD_FILE_MUST_EXIST);
+
+    if (openFileDialog.ShowModal() == wxID_CANCEL) {
+        return;
+    }
+
+    wxString filePath = openFileDialog.GetPath();
+    wxMessageBox("Selected file:\n" + filePath, "File Path");
+}
+
+// Exit
+void MyFrame::OnExit(wxCommandEvent& event)
+{
+    Close(true);
+}
